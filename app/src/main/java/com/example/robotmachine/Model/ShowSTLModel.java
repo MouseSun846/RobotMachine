69d6c859095cc6b6c63211ab163809d91d13bccf
MouseSun <3026098675@qq.com>
2019-08-04 15:15:19 +0800

    六轴联动

--- app/src/main/java/com/example/robotmachine/Model/ShowSTLModel.java
+++ app/src/main/java/com/example/robotmachine/Model/ShowSTLModel.java
@@ -8,54 +8,39 @@ import com.example.robotmachine.Util.Point;
 import javax.microedition.khronos.opengles.GL10;
 
 public class ShowSTLModel {
-    private Point eye;
-    private Point up;
-    private Point center;
-    private float scale;
-    //模型平移位置
-    private Point movPos = new Point(0f,0f,0f);
-    public ShowSTLModel(Point eye,Point up,Point center,float scale){
-        this.eye = eye;
-        this.up = up;
-        this.center = center;
-        this.scale = scale;
-    }
+
     public void showModel(GL10 gl,Model model){
-        //重置当前模型矩阵
-        gl.glLoadIdentity();
-        //眼睛对着原点看
-        GLU.gluLookAt(gl,eye.x,eye.y,eye.z,center.x,center.y,center.z,up.x,up.y,up.z);
-        //注意坐标轴也选择了（逆时针）
-        gl.glRotatef(180f, 1f, 0f, 0f);
+
         //模型旋转
         int order = model.getOrder();
-        if (order == 1){
-            gl.glRotatef(90f,1,0,0);
-        }else if (order == 2){
-            gl.glRotatef(180f,1,0,0);
-        }
-        gl.glRotatef(model.mDegree.x,1,0,0);
 
-        //将模型放缩到View刚好装下
-        gl.glScalef(scale,scale,scale);
-        //获得模型的中心点
-        Point centerPoint = model.getCenterPoint();
-        movPos.x = movPos.x + centerPoint.x;
-        movPos.y = movPos.y + centerPoint.y;
-        movPos.z = movPos.z + centerPoint.z;
-
-        //拼接模型
         if (order == 1) {
-            gl.glTranslatef(0, 0,-0.4f);
-        }else if(order == 2){
-            gl.glTranslatef(0, -0.17f,0);
-        } else if (order == 6) {
-            gl.glTranslatef(0.3f, -movPos.y,0);
-        } else if (order > 6) {
-            gl.glTranslatef(0.38f, -0.15f, 0);
-        } else {
-            gl.glTranslatef(0, -movPos.y, 0);
+            gl.glTranslatef(0, -0.4f,0.3f);
+        }else if (order == 2){
+            gl.glTranslatef(0f, 0f,0.26f);
+        }else if (order == 3){
+            gl.glTranslatef(0f, 0f,0f);
+        }else if (order == 4){
+            gl.glTranslatef(0f, 0.35f,0f);
+        }else if (order == 5){
+            gl.glTranslatef(0.08f, 0f,0f);
+        }else if (order == 6){
+            gl.glTranslatef(0.27f, 0f,0f);
+        }else if (order == 7){
+            gl.glTranslatef(0.06f, 0f,0f);
         }
+
+
+        if (order == 1){
+            gl.glRotatef(-90f,1,0,0);
+        }else if (order == 2){
+            gl.glRotatef(90f,1,0,0);
+        }
+            gl.glRotatef(model.mDegree.x, 1, 0, 0);
+            gl.glRotatef(model.mDegree.y, 0, 1, 0);
+            gl.glRotatef(model.mDegree.z, 0, 0, 1);
+
+
 
         //***************begin***************//
         //允许给每个顶点设置法向量
@@ -75,6 +60,7 @@ public class ShowSTLModel {
         //***************end***************//
         gl.glFinish();
     }
+
 
 
 }
--- app/src/main/java/com/example/robotmachine/OpenGL/GLRenderer.java
+++ app/src/main/java/com/example/robotmachine/OpenGL/GLRenderer.java
@@ -30,26 +30,29 @@ public class GLRenderer implements GLSurfaceView.Renderer {
     private Point center = new Point(0,0,0);
     private float mScalef = 1;
     private MouseRotate mouseRotate = new MouseRotate(0,0);
-    private Point moveDistance = new Point(0f,0f,0f);
     //纹理
     private float cnt=0;
     private Context context;
     private List<Model> models = new ArrayList<>();
 
-    public GLRenderer(Context context){
-        try {
-            String[] link = context.getResources().getStringArray(R.array.link);
-            for (int i = 0; i < link.length; i++) {
-                Model model = new STLReader().parseBinStlInAssets(context,link[i]);
-                model.setOrder(i+1);
-                models.add(model);
-                Log.i("mouse","i = "+i);
-            }
+    public GLRenderer(final Context context){
 
+        initSixAxisesView(context);
+
+        this.context = context;
+    }
+    private void initSixAxisesView(Context context){
+        String[] link = context.getResources().getStringArray(R.array.link);
+        try {
+        for (int i = 0; i < link.length; i++) {
+            Model model = new STLReader().parseBinStlInAssets(context,link[i]);
+            model.setOrder(i+1);
+            models.add(model);
+            Log.i("mouse","i = "+i);
+        }
         }catch (Exception e){
             e.printStackTrace();
         }
-        this.context = context;
     }
 
     @Override
@@ -63,16 +66,15 @@ public class GLRenderer implements GLSurfaceView.Renderer {
         gl.glDepthFunc(GLES10.GL_LEQUAL);
         //设置阴影模式
         gl.glShadeModel(GLES10.GL_SMOOTH);
+        //所有的变换都针对于模型
+        gl.glMatrixMode(GL10.GL_MODELVIEW);
         float r = Util.getR(models);
         //r 是半径，因此用0.5/r计算缩放比例
         mScalef = 0.5f / r;
-//        mCenterPoint = Util.getCenter(models);
-//        Log.i("mouse",""+mCenterPoint.x+"  "+mCenterPoint.y+"  "+mCenterPoint.z);
         //开启光
         openLight(gl);
         //添加材质属性
         enableMaterial(gl);
-//        texture = TextureHelper.loadTexture(context, R.drawable.girl);
     }
 
     @Override
@@ -94,29 +96,48 @@ public class GLRenderer implements GLSurfaceView.Renderer {
     public void onDrawFrame(GL10 gl) {
         // 清除屏幕和深度缓存
         gl.glClear(GL10.GL_COLOR_BUFFER_BIT|GL10.GL_DEPTH_BUFFER_BIT);
-        ShowSTLModel showSTLModel = new ShowSTLModel(eye,up,center,mScalef);
+        gl.glMatrixMode(GL10.GL_MODELVIEW);
+        //重置当前模型矩阵
+        gl.glLoadIdentity();
+        //眼睛对着原点看
+        GLU.gluLookAt(gl,eye.x,eye.y,eye.z,center.x,center.y,center.z,up.x,up.y,up.z);
+        //注意坐标轴也选择了（逆时针）
+//        gl.glRotatef(90f, 1f, 0f, 0f);
+        //将模型放缩到View刚好装下
+        gl.glScalef(mScalef,mScalef,mScalef);
+        ShowSTLModel showSTLModel = new ShowSTLModel();
         modelControlState();
-        for (Model model : models) {
-            showSTLModel.showModel(gl,model);
+        if (models.size() == 9 ) {
+            for (Model model : models) {
+                showSTLModel.showModel(gl, model);
+            }
         }
     }
-    float[] ambient = {0.9f, 0.9f, 0.9f, 1.0f,};
-    float[] diffuse = {0.5f, 0.5f, 0.5f, 1.0f,};
+    float[] ambient = {0.0f, 0.0f, 0.0f, 1.0f,};
+    float[] diffuse = {1.0f, 1.0f, 1.0f,1.0f,};
     float[] specular = {1.0f, 1.0f, 1.0f, 1.0f,};
     float[] lightPosition = {0.5f, 0.5f, 0.5f, 0.0f,};
 
     public void openLight(GL10 gl) {
         gl.glEnable(GL10.GL_LIGHTING);
         gl.glEnable(GL10.GL_LIGHT0);
+        gl.glShadeModel(GL10.GL_SMOOTH);
+        //RGBA模式下环境光
         gl.glLightfv(GL10.GL_LIGHT0, GL10.GL_AMBIENT, Util.floatToBuffer(ambient));
+        //RGBA模式下漫反射光
         gl.glLightfv(GL10.GL_LIGHT0, GL10.GL_DIFFUSE, Util.floatToBuffer(diffuse));
+        //RGBA模式下镜面光
         gl.glLightfv(GL10.GL_LIGHT0, GL10.GL_SPECULAR, Util.floatToBuffer(specular));
+        //光源位置齐次坐标
         gl.glLightfv(GL10.GL_LIGHT0, GL10.GL_POSITION, Util.floatToBuffer(lightPosition));
+
     }
 
-    float[] materialAmb = {1.0f, 0.5f, 0.27f, 1.0f};
-    float[] materialDiff = {1.0f, 0.5f, 0.27f, 1.0f};//漫反射
-    float[] materialSpec = {1.0f, 0.5f, 0.27f, 1.0f};
+    float[] materialAmb = {1.0f, 0.6f, 0.3f, 1.0f};
+    float[] materialDiff = {1.0f, 0.6f, 0.3f, 1.0f};//漫反射
+    float[] materialSpec = {1.0f, 0.6f, 0.3f,  1.0f};
+    //衰减系数，越小越亮
+    float[] shiness = {0.8f};
 
     public void enableMaterial(GL10 gl) {
 
@@ -126,7 +147,8 @@ public class GLRenderer implements GLSurfaceView.Renderer {
         gl.glMaterialfv(GL10.GL_FRONT_AND_BACK, GL10.GL_DIFFUSE, Util.floatToBuffer(materialDiff));
         //镜面光的反射情况
         gl.glMaterialfv(GL10.GL_FRONT_AND_BACK, GL10.GL_SPECULAR, Util.floatToBuffer(materialSpec));
-
+        //光亮度
+        gl.glMaterialfv(GL10.GL_FRONT_AND_BACK,GL10.GL_SHININESS,Util.floatToBuffer(shiness));
     }
 
     //按下时候坐标
@@ -141,8 +163,6 @@ public class GLRenderer implements GLSurfaceView.Renderer {
 //        Log.i("mouse","---Drag--- "+"X: "+normalizex+" Y: "+normalizey);
         mouseRotate.xDegree = (normalizex-mouseRotate.preX)*360;
         mouseRotate.yDegree = (normalizey-mouseRotate.preY)*360;
-        moveDistance.x = normalizex - mouseRotate.preX;
-        moveDistance.y = normalizey - mouseRotate.preY;
     }
 
     //RadioButton 监听接口
@@ -154,29 +174,32 @@ public class GLRenderer implements GLSurfaceView.Renderer {
     public void modelControlState(){
         switch (modelId){
             case R.id.rbAxiseAll:
-                for (Model model:models) {
-                    model.mDegree.x = mouseRotate.xDegree;
-                }
+                models.get(0).mDegree.x=mouseRotate.xDegree;
+
                 break;
             case R.id.rbAxiseOne:
+                models.get(1).mDegree.y=mouseRotate.xDegree;
 
                 break;
             case R.id.rbAxiseTwo:
+                models.get(2).mDegree.z=mouseRotate.xDegree;
 
                 break;
             case R.id.rbAxiseThree:
+                models.get(3).mDegree.z=mouseRotate.xDegree;
 
                 break;
             case R.id.rbAxiseFour:
-
+                models.get(4).mDegree.x=mouseRotate.xDegree;
                 break;
             case R.id.rbAxiseFive:
-
+                models.get(5).mDegree.z=mouseRotate.xDegree;
                 break;
             case R.id.rbAxiseSix:
 
                 break;
         }
+//        Log.i("mouse","xDegree  "+mouseRotate.xDegree);
     }
 
 }
